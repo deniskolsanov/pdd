a32bda37ac064e5d3997cd69bdca63539123e5a1
Opera <deniskolsanov@gmail.com>
2020-06-24 19:40:57 +0300

    qwe2

---
+++ client/build/precache-manifest.087102edf765f66a846e371b15861202.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc407668b4e953c9be3a54cc97483316",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d76ad36d498c883fb91",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7b7b8c76f2acfb258b8a",
+    "url": "/static/css/main.03732058.chunk.css"
+  },
+  {
+    "revision": "0d76ad36d498c883fb91",
+    "url": "/static/js/2.ee8d0518.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.ee8d0518.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b7b8c76f2acfb258b8a",
+    "url": "/static/js/main.664c5910.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
---
+++ client/build/precache-manifest.ff7f904ee1e7b2593fd6c8e62107a14e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "840be0d6895c35013acb0b9e2c60eb27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6822f6665d4efd44dc22",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c097e9b0c76321a39cc8",
+    "url": "/static/css/main.03732058.chunk.css"
+  },
+  {
+    "revision": "6822f6665d4efd44dc22",
+    "url": "/static/js/2.e3cb76eb.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.e3cb76eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c097e9b0c76321a39cc8",
+    "url": "/static/js/main.3d2bb33e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
